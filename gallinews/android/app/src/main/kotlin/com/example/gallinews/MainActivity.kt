9e861b86315d854e2ceb1d92cf1c1871a3186dae
Hardik Pithadia <hardikrpithadia@gmail.com>
2022-08-06 10:02:00 +0530

    Project Added

---
+++ gallinews/android/app/src/main/kotlin/com/example/gallinews/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gallinews
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ gallinews/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("GalliNews"),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+}
